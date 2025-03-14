a629141eacc2c228b9b9d025e8e7d28011b5cfd3
GinaMincu <mincugina72@yahoo.com>
2025-03-14 14:34:35 +0200

    added services like add vehicle and update vehicle

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -17,6 +17,50 @@ export const getVehicles = async () => {
   }
 };
 
+
+export const addVehicle = async (newVehicle) => {
+
+  try {
+    const response = await fetch(`${API_URL}/api/vehicles/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",  // Required for JSON payload
+      },
+      body: JSON.stringify(newVehicle) // Convert object to JSON string
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
+};
+
+export const updateVehicle = async (vehicleId,updatedVehicle) => {
+  try {
+    const response = await fetch(`${API_URL}/api/vehicles/${vehicleId}/`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",  // Required for JSON payload
+      },
+      body: JSON.stringify(updatedVehicle) // Convert object to JSON string
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
+};
+
 export const deleteVehicle = async (vehicleId) => {
   try {
     const response = await fetch(`${API_URL}/api/vehicles/${vehicleId}/`, {
